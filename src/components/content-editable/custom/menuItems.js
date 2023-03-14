06bbc3c05befbb3ccecd6fe3a4582b287aff9978
Aman <webdevaman2@gmail.com>
2023-03-14 16:43:25 +0530

    seperate toggleAlignment file

--- src/components/content-editable/custom/menuItems.js
+++ src/components/content-editable/custom/menuItems.js
@@ -7,6 +7,7 @@ import toggleHeading from "../utils/ToggleHeading";
 
 import {findParentNodeOfType} from 'prosemirror-utils';
 import toggleCodeBlock from "../utils/ToggleCodeBlock";
+import { toggleAlignment } from "../utils/ToggleAlignment";
 
 let markType = {
   strong: schema.marks.strong,
@@ -285,83 +286,7 @@ const headingTwoItem = new MenuItem({
 });
 
 /* ALIGN ITEM CENTER */
-export function setTextAlign(
-  tr,
-  schema,
-  alignment
-) {
-  const {selection, doc} = tr;
-  if (!selection || !doc) {
-    return tr;
-  }
-  const {from, to} = selection;
-  const {nodes} = schema;
-
-  // const blockquote = nodes[BLOCKQUOTE];
-  // const listItem = nodes[LIST_ITEM];
-  const heading = nodes[HEADING];
-  const paragraph = nodes[PARAGRAPH];
-
-  const tasks = [];   
-  const tasks2 = []; // for toggle
-  alignment = alignment || null;
-
-  const allowedNodeTypes = new Set([ heading, paragraph]);
-
-  doc.nodesBetween(from, to, (node, pos, parentNode) => {
-    const nodeType = node.type;
-    const align = node.attrs.align || null;
-    if (align !== alignment && allowedNodeTypes.has(nodeType)) {
-      tasks.push({
-        node,
-        pos,
-        nodeType,
-      });
-    }else if(allowedNodeTypes.has(nodeType)){
-      tasks2.push({
-        node,
-        pos,
-        nodeType,
-      });
-    }
-    return true;
-  });
-
-  if (tasks2.length) {
-    tasks2.forEach(job => {
-      const {node, pos, nodeType} = job;
-      let {attrs} = node;
-      if (alignment)   {
-        attrs = {
-          ...attrs,
-          align: null,
-        };
-      }
-      tr = tr.setNodeMarkup(pos, nodeType, attrs, node.marks);
-    });
-    return tr;
-  }
-
-  tasks.forEach(job => {
-    const {node, pos, nodeType} = job;
-    let {attrs} = node;
-    if (alignment) {
-      attrs = {
-        ...attrs,
-        align: alignment,
-      };
-    } else {
-      attrs = {
-        ...attrs,
-        align: null,
-      };
-    }
-    tr = tr.setNodeMarkup(pos, nodeType, attrs, node.marks);
-  });
-
-  return tr;
-}
-
+ 
 const alignCenterItem = new MenuItem({
   label: "align center",
   enable(state) {
@@ -369,11 +294,8 @@ const alignCenterItem = new MenuItem({
   },
 
   run(state, dispatch, view) {
-    // const nodeType = state.schema.nodes.paragraph;
-    // const attrs = { align: "center" };
-    // setBlockType(nodeType, attrs)(state, dispatch);
     const {schema, selection} = state;
-    const tr = setTextAlign(
+    const tr = toggleAlignment(
       state.tr.setSelection(selection),
       schema,
       'center'
