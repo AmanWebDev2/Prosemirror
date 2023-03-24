41131ac928bd021efbd904cadfad1eede22939db
Aman <webdevaman2@gmail.com>
2023-03-24 11:19:02 +0530

    removed focus plugin : causes issue when link is clicked

--- src/components/content-editable/plugins/plugins.js
+++ src/components/content-editable/plugins/plugins.js
@@ -3,15 +3,15 @@ import { selectionMenu } from "./selectionMenu";
 import { keymap } from "prosemirror-keymap";
 import { undo, redo, history } from "prosemirror-history";
 import { baseKeymap } from "prosemirror-commands";
-import SelectionPlaceholderPlugin from "./SelectionPlaceholderPlugin";
 import { editorUpdateObserver } from "./editorUpdateObserver";
 import { tooltipMenuItems } from "../custom/menu/menuItems";
-import { rulseSetAttribute } from "./rulseSetAttribute";
 import { buildKeymap } from "./keyMap";
 import { prosmirrorSchema } from "../custom/schema/schema";
+import { rulseSetAttribute } from "./rulseSetAttribute";
+import SelectionPlaceholderPlugin from "./SelectionPlaceholderPlugin";
 
 export const plugins = [
-  new SelectionPlaceholderPlugin(),
+  // new SelectionPlaceholderPlugin(),
   // rulseSetAttribute(),
   history(),
   keymap({ "Mod-z": undo, "Mod-y": redo }),
