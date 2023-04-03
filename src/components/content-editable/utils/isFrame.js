ce1e4baaacc33f06c94d2981ba195f5729b20c39
Aman <webdevaman2@gmail.com>
2023-04-03 13:12:15 +0530

    added isIframe method

---
+++ src/components/content-editable/utils/isFrame.js
@@ -0,0 +1,10 @@
+const checkForIframe =()=> {
+    return true
+    // let iframe = document.querySelector('iframe');
+    // if(!iframe) return false;
+    // iframe = document.getElementById('kudoshub-editor-frame');
+    // if(!iframe) return false;
+    // return true;
+}
+
+export const isIframe = checkForIframe();
