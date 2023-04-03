8a64046a854cb976d41f5db4666bd283ebe54012
Aman <webdevaman2@gmail.com>
2023-04-03 11:07:46 +0530

    changed to functional component

--- src/components/emoji/EmojiResultRow.js
+++ src/components/emoji/EmojiResultRow.js
@@ -1,32 +1,26 @@
 import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-export default class EmojiResultsRow extends PureComponent {
-  static propTypes = {
-    title: PropTypes.string,
-    symbol: PropTypes.string,
-    group: PropTypes.string
-  };
-  getEmoji(e) {
-    this.props.getEmoji(e);
-  }
 
-  render() {
-    const emojiStyle = {
-      fontFamily: `Apple Color Emoji,Segoe UI Emoji,NotoColorEmoji, Segoe UI Symbol,Android Emoji, EmojiSymbols,sans-serif`,
-      fontSize: `25px`,display: `inline-table`
-    };
-    return (
-      <span
-        className="kudosHub-emoji-picker"
-        title={this.props.title}
-        aria-label={this.props.title}
-        role="button"
-        tabIndex="0"
-        onClick={e => this.getEmoji(e)}
-        style={emojiStyle}
-      >
-        {this.props.symbol}
-      </span>
-    );
+const EmojiResultRow=(props)=>{
+  const emojiStyle = {
+    fontFamily: `Apple Color Emoji,Segoe UI Emoji,NotoColorEmoji, Segoe UI Symbol,Android Emoji, EmojiSymbols,sans-serif`,
+    fontSize: `25px`,display: `inline-table`
+  };
+  const getEmoji=(e)=>{
+    props.getEmoji(e);
   }
+  return (
+    <span
+      className="kudosHub-emoji-picker"
+      title={props.title}
+      aria-label={props.title}
+      role="button"
+      tabIndex="0"
+      onClick={e => getEmoji(e)}
+      style={emojiStyle}
+    >
+      {props.symbol}
+    </span>
+  );
 }
+
+export default EmojiResultRow;
