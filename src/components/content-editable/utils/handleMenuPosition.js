e0521b0abea387bb2490445e603ff46aec5e22f5
Aman <webdevaman2@gmail.com>
2023-04-05 16:22:37 +0530

    formatted code

--- src/components/content-editable/utils/handleMenuPosition.js
+++ src/components/content-editable/utils/handleMenuPosition.js
@@ -1,38 +1,59 @@
 import { markActive } from "./markActive";
 
-export const handleMenuPosition=({view,isLinkActive,iframe,menu})=> {
-    const { state, readOnly } = view;
-    const { from, to } = state.selection;
-    const start = view.coordsAtPos(from);
-    const end = view.coordsAtPos(to);
+export const handleMenuPosition = ({ view, isLinkActive, iframe, menu }) => {
+  const { state, readOnly } = view;
+  const { from, to } = state.selection;
+  const start = view.coordsAtPos(from);
+  const end = view.coordsAtPos(to);
 
+  const domNode = view.nodeDOM(from); // Get the corresponding DOM node
 
-     try {
-      let box = menu.getBoundingClientRect();
-
-      let offsetParentBox = menu.offsetParent ? menu.offsetParent.getBoundingClientRect():document.body.getBoundingClientRect();
-      let left =
-        (start.left + end.left) / 2 - box.width / 2 - offsetParentBox.left;
-      if (left < 5) {
-        left = 5;
-      }
-      
-      if (((markActive(state, state.schema.marks.link) && !(!state || readOnly || state.selection.from !== state.selection.to) )|| isLinkActive)) {
-        if(iframe) {
-          menu.style.top = (start.bottom)+"px"
-          menu.style.left = left + "px"
-        }else {
-          menu.style.transform = `translate(${left}px,${start.bottom}px)`; 
-        }
-      } else {
-        if(iframe) {
-          menu.style.top = (start.top - offsetParentBox.top - box.height)+"px"
-          menu.style.left = left + "px";
-        }else {
-          menu.style.transform = `translate(${left}px,${(start.top - offsetParentBox.top - box.height)}px)`;  
-        }
-      }
-    } catch (err) {
-        console.error(err);
+  try {
+    let box = menu.getBoundingClientRect();
+    let offsetParentBox = menu.offsetParent ? menu.offsetParent.getBoundingClientRect() : document.body.getBoundingClientRect();
+    let left = (start.left + end.left) / 2 - box.width / 2 - offsetParentBox.left;
+    if (left < 5) {
+      left = 5;
     }
+    if (
+      (
+        markActive(state, state.schema.marks.link) && !(!state || readOnly || state.selection.from !== state.selection.to)) ||
+        isLinkActive
+      ) {
+      if (iframe) {
+        menu.style.top = start.bottom + "px";
+        menu.style.left = left + "px";
+      } else {
+        menu.style.transform = `translate(${left}px,${start.bottom}px)`;
+      }
+    } else {
+      if (iframe) {
+        menu.style.top = start.top - offsetParentBox.top - box.height + "px";
+        menu.style.left = left + "px";
+      } else {
+        // if(domNode && domNode.nodeName !== 'SPAN') {
+        //   const domNodeRect = domNode.getBoundingClientRect();
+        //   menu.style.transform = `translate(${left + domNodeRect.width/2}px,${(start.top - offsetParentBox.top - box.height)}px)`;
+        // }else {
+        // }
+        if (
+          domNode &&
+          domNode.nodeName === "DIV" &&
+          domNode.classList.contains("image-container")
+        ) {
+          const domNodeRect = domNode.getBoundingClientRect();
+          console.log("positioned");
+          menu.style.transform = `translate(${domNodeRect.width / 2 + left}px,${
+            start.top - offsetParentBox.top - box.height
+          }px)`;
+        } else {
+          menu.style.transform = `translate(${left}px,${
+            start.top - offsetParentBox.top - box.height
+          }px)`;
+        }
+      }
+    }
+  } catch (err) {
+    console.error(err);
   }
+};
